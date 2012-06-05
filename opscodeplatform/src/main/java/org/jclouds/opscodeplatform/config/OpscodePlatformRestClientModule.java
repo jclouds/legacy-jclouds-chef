e237f91a902a0a5f06d1aa301ea616bd14ff039e
Ignasi Barrera <ignasi.barrera@abiquo.com>
2012-06-05 12:49:51 +0200

    Upgraded surefire plugin version. Fixes #13
    Upgrading surefire plugin to version 2.12 fixes issue #13.
    Also refactored RestClientModule to config package and user
    and organization functions to the functions package.

--- opscodeplatform/src/main/java/org/jclouds/opscodeplatform/config/OpscodePlatformRestClientModule.java
+++ opscodeplatform/src/main/java/org/jclouds/opscodeplatform/config/OpscodePlatformRestClientModule.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.jclouds.opscodeplatform.functions;
+package org.jclouds.opscodeplatform.config;
 
 import java.util.Map;
 
